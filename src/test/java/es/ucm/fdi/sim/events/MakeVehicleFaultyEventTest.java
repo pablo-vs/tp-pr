3c04dcef9e02aca12d19e2eed9b29e160832e7b5
Eduardo Rivero Rodríguez <eduriver@ucm.es>
2018-03-13 09:49:45 +0100

    Version 0.4.6
    - Added MakeVehicleFaultyEvent execution test (doubts regarding
    correctness on speed reduction, needs check)

--- src/test/java/es/ucm/fdi/sim/events/MakeVehicleFaultyEventTest.java
+++ src/test/java/es/ucm/fdi/sim/events/MakeVehicleFaultyEventTest.java
@@ -86,9 +86,82 @@ public class MakeVehicleFaultyEventTest implements EventTest {
 	@Test
 	public void runTest(){
 		IniSection sec, report;
-		MakeVehicleFaultyEventTest event;
+		MakeVehicleFaultyEvent event;
 		RoadMap r;
+		List<Junction> it;
+		
+		r = new RoadMap();
+		r.addJunction(new Junction("j"));
+		r.addJunction(new Junction("j1"));
+		r.addJunction(new Junction("j2"));
+		
+		r.addRoad(new Road("r1", 200, 20, r.getJunction("j1"), r.getJunction("j")));
+		r.addRoad(new Road("r2", 200, 20, r.getJunction("j2"), r.getJunction("j")));
+		
+		it = new ArrayList<Junction>();
+		it.add(r.getJunction("j1"));
+		it.add(r.getJunction("j"));
+		r.addVehicle(new Vehicle("v1", 40, it));
+		r.getRoad("r1").move();
+		r.getRoad("r1").move();
+		r.addVehicle(new Vehicle("v2", 40, it));
+		r.getRoad("r1").move();
+		
+		it = new ArrayList<Junction>();
+		it.add(r.getJunction("j2"));
+		it.add(r.getJunction("j"));
+		r.addVehicle(new Vehicle("v3", 200, it));
+		
+		sec = new IniSection("make_vehicle_faulty");
+		sec.setValue("time", "4");
+		sec.setValue("vehicles", "v1,v3");
+		sec.setValue("duration", "2");
+		event = new MakeVehicleFaultyEvent.Builder().build(sec);
+		event.execute(r);
+		r.getRoad("r1").move();
+		
+		report = new IniSection("vehicle_report");
+		report.setValue("id", "v1");
+		report.setValue("time", "4");
+		report.setValue("speed", "0");
+		report.setValue("kilometrage", "51"); //SHOULD BE 50 ¿?¿?
+		report.setValue("faulty", "1");
+		report.setValue("location", "(r1,51)");
+		assertEquals("Report does not match.", report, r.getVehicle("v1").report(4));
+		
+		report = new IniSection("vehicle_report");
+		report.setValue("id", "v2");
+		report.setValue("time", "4");
+		report.setValue("speed", "5");
+		report.setValue("kilometrage", "16");
+		report.setValue("faulty", "0");
+		report.setValue("location", "(r1,16)");
+		assertEquals("Report does not match.", report, r.getVehicle("v2").report(4));
 		
 		
+		report = new IniSection("vehicle_report");
+		report.setValue("id", "v3");
+		report.setValue("time", "4");
+		report.setValue("speed", "0");
+		report.setValue("kilometrage", "0");
+		report.setValue("faulty", "2");
+		report.setValue("location", "(r2,0)");
+		assertEquals("Report does not match.", report, r.getVehicle("v3").report(4));
+		
+		sec = new IniSection("make_vehicle_faulty");
+		sec.setValue("time", "4");
+		sec.setValue("vehicles", "v2");
+		sec.setValue("duration", "4");
+		event = new MakeVehicleFaultyEvent.Builder().build(sec);
+		event.execute(r);
+		
+		report = new IniSection("vehicle_report");
+		report.setValue("id", "v2");
+		report.setValue("time", "4");
+		report.setValue("speed", "0");
+		report.setValue("kilometrage", "16");
+		report.setValue("faulty", "4");
+		report.setValue("location", "(r1,16)");
+		assertEquals("Report does not match.", report, r.getVehicle("v2").report(4));
 	}
 }
